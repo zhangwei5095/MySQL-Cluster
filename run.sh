5c98665dd77fbd01c130fe4b9d520128ecadd098
Henrik Sachse <t3x7m3@posteo.de>
2015-08-13 11:16:55 +0200

    Using 'exec' in the run script to keep those started processes as the parent process with PID 1 in the container.

--- run.sh
+++ run.sh
@@ -28,14 +28,14 @@ echo "Using management server ${MYSQL_MANAGEMENT_SERVER}"
 
 if [ $1 == "ndbd" ]; then
     echo "Starting ndbd..."
-    ${MYSQL_CLUSTER_BIN}/ndbd --nodaemon ${INITIAL} --connect-string="host=${MYSQL_MANAGEMENT_SERVER}:${MYSQL_MANAGEMENT_PORT}"
+    exec ${MYSQL_CLUSTER_BIN}/ndbd --nodaemon ${INITIAL} --connect-string="host=${MYSQL_MANAGEMENT_SERVER}:${MYSQL_MANAGEMENT_PORT}"
 elif [ $1 == "ndb_mgmd" ]; then
     echo "Starting ndb_mgmd..."
-    ${MYSQL_CLUSTER_BIN}/ndb_mgmd --nodaemon ${RELOAD} ${INITIAL} -f ${MYSQL_CLUSTER_CONFIG} # --no-nodeid-checks --ndb-nodeid=1
+    exec ${MYSQL_CLUSTER_BIN}/ndb_mgmd --nodaemon ${RELOAD} ${INITIAL} -f ${MYSQL_CLUSTER_CONFIG} # --no-nodeid-checks --ndb-nodeid=1
 elif [ $1 == "mysqld" ]; then
     echo "Starting mysqld_safe..."
-    ${MYSQL_CLUSTER_BIN}/mysqld_safe --ndbcluster --ledir=${MYSQL_CLUSTER_BIN} --ndb-connectstring=${MYSQL_MANAGEMENT_SERVER} # --skip-grant-tables
+    exec ${MYSQL_CLUSTER_BIN}/mysqld_safe --ndbcluster --ledir=${MYSQL_CLUSTER_BIN} --ndb-connectstring=${MYSQL_MANAGEMENT_SERVER} # --skip-grant-tables
 else
     echo "Starting ndb_mgm..."
-    ${MYSQL_CLUSTER_BIN}/ndb_mgm ${MYSQL_MANAGEMENT_SERVER}
+    exec ${MYSQL_CLUSTER_BIN}/ndb_mgm ${MYSQL_MANAGEMENT_SERVER}
 fi
